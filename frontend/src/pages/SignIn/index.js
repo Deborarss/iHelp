0d3ac472929aed56a6f8ed548564b0ad75d16e00
Debora Souza <debora.souza@wunderman.com>
2020-04-20 17:01:26 -0300

    Handle with forms using unform lib

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/info.svg';
 
 export default function SignIn() {
+
+  const handleSubmit = data => {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="iHelp"/>
 
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   )
 }
