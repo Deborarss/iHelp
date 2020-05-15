5c9f86123c382afc7c00d7704a4136fc887702dc
Debora Souza <debora_rss@hotmail.com>
2020-05-15 14:25:25 -0300

    Create table of appointments

---
+++ backend/src/database/migrations/1589549094984-CreateAppointments.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1589549094984
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'helper',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+          {
+            name: 'place',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'details',
+            type: 'text',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
