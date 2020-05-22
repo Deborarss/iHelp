92bcae05f28e7faacbc4139c87c9752e2d0df99f
Debora Souza <debora_rss@hotmail.com>
2020-05-22 14:47:12 -0300

    Config authenticated routes and add user interface to request(express)

---
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+interface Teste {
+  secret: string;
+}
+
+export default function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('JWT token is missing');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  const { secret } = authConfig.jwt;
+
+  try {
+    const decoded = verify(token, secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    req.user = {
+      id: sub,
+    };
+
+    console.log(decoded);
+    return next();
+  } catch {
+    throw new Error('Invalid JWT token');
+  }
+}
