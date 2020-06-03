ebbdab639437b7cf1f03311f67d1aecbc5ed546c
Deborarss <debora_rss@hotmail.com>
2020-06-03 18:13:49 -0300

    Enable API cors, create and configure Auth Context

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -11,6 +12,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
